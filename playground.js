05af073a46c91f8f3dc5df37dbfc87d5ae8319bb
MUEID11 <mehedirangpur3@gmail.com>
2024-11-04 13:08:26 +0600

    revising

---
+++ playground.js
@@ -0,0 +1,27 @@
+//ES6 fat arrow function
+// let number = () => 10;
+// console.log(number())
+const searchInput = document.querySelector(".search");
+const display = document.querySelector(".result");
+const thanks = document.querySelector(".thanks");
+
+// function show(){
+//   display.innerHTML = this.value;
+//   var self = this;
+//   setTimeout(function(){
+//     thanks.innerHTML = `you have typed ${self.value}`;
+//   },1000)
+// };
+
+// const showArrow = () => {
+//   console.log(this)
+//   display.innerHTML = this.value
+//   setTimeout(() => (thanks.innerHTML = `you have typed: ${this.value}`),1000);
+// };
+
+//Arrow doesnt care about (this).
+//use normal function for get outer (thi)s like an event, and to avoid confusion you can use arrow function on object to get the (this). because normal function changes the this, you can also do it by 
+//var self = this; in  a normal function.
+
+
+searchInput.addEventListener("keyup", showArrow);
