d22b7c0ee841548c9a608a28246297afc6045997
MUEID11 <mehedirangpur3@gmail.com>
2024-11-14 15:45:15 +0600

    finished 3.8

--- React/practice-documentation/src/Travel.jsx
+++ React/practice-documentation/src/Travel.jsx
@@ -6,13 +6,32 @@ const Travel = () => {
   const root = plan[0];
 
   const planetIds = root.childIds;
+  const handleComplete = (parentId, childId) => {
+    const parent = plan[parentId];
+    
+    const nextParent = {
+      ...parent,
+      childIds: parent.childIds.filter((id) => id !== childId),
+    };
+    setPlan({
+      ...plan,
+      [parentId]: nextParent,
+      
+    });
+  };
 
   return (
     <div>
       <h2>Places to visit</h2>
       <ol>
         {planetIds.map((placeId) => (
-          <TravelTree key={placeId} id={placeId} placesById={plan} />
+          <TravelTree
+            onComplete={handleComplete}
+            key={placeId}
+            id={placeId}
+            placesById={plan}
+            parentId={0}
+          />
         ))}
       </ol>
     </div>
--- React/practice-documentation/src/TravelTree.jsx
+++ React/practice-documentation/src/TravelTree.jsx
@@ -1,18 +1,25 @@
 /* eslint-disable react/prop-types */
-export default function TravelTree({ placesById, id }){
-  const place = placesById[id]
+export default function TravelTree({ id, placesById, parentId, onComplete }) {
+  const place = placesById[id];
   const childIds = place.childIds;
- 
-    return (
-        <div>
-          {place.title}
-          {
-            childIds.length > 0 && (
-              <ol>
-                {childIds.map(place => <TravelTree key={place.id} id={place.id}  placesById={placesById} />)}
-              </ol>
-            )
-          }
-        </div>
-    );
+
+  return (
+    <div>
+      {place.title}{" "}
+      <button onClick={() => onComplete(parentId, id)}>Complete</button>
+      {childIds.length > 0 && (
+        <ol>
+          {childIds.map((childId) => (
+            <TravelTree
+              key={childId}
+              id={childId}
+              placesById={placesById}
+              onComplete={onComplete}
+              parentId={id}
+            />
+          ))}
+        </ol>
+      )}
+    </div>
+  );
 }
