5b27904699d17ac8867978a191a02e9371ce71d6
MUEID11 <mehedirangpur3@gmail.com>
2024-11-21 15:48:24 +0600

    context passing through same component

--- React/practice-documentation/src/App.jsx
+++ React/practice-documentation/src/App.jsx
@@ -1,14 +1,31 @@
-import Tasklist from "./Tasklist/Tasklist"
+import Heading from "./Heading"
+import Section from "./Section"
+
 
 
 function App() {
   
 
   return (
-    <>
-     <Tasklist />
-    </>
-  )
+    <Section>
+      <Heading>Title</Heading>
+      <Section>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
 }
 
 export default App
